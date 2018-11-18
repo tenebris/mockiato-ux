fd8545e511d084c24e5245cd5e2421c64929dcac
Robert H. Hughes <robert@tenebris.us>
2018-11-18 15:37:07 -0600

    generated files

---
+++ src/app/show-if-authenticated.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShowIfAuthenticatedDirective } from './show-if-authenticated.directive';
+
+describe('ShowIfAuthenticatedDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShowIfAuthenticatedDirective();
+    expect(directive).toBeTruthy();
+  });
+});
