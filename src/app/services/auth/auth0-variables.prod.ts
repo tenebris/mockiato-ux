b98d769413d3dc488d7f473ca3f265fb116b0ff3
Robert H. Hughes <robert@tenebris.us>
2018-11-18 20:23:15 -0600

    adjusted 'prod' url for ngrok until production instance/deploy ready

--- src/app/services/auth/auth0-variables.prod.ts
+++ src/app/services/auth/auth0-variables.prod.ts
@@ -8,5 +8,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'VL0mykQ9D2x2qmVuAcsitlq8NQp2tshE',
   domain: 'tenebris.auth0.com',
-  callbackURL: 'https://mockiato.tenebris.us/callback'
+  callbackURL: 'https://tenebris-mockiato-ux.ngrok.io/callback'
 };
