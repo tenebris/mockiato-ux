4e68eeac1df7fe83b62e912d904868a3e49b00aa
Robert Hughes <robert@tenebris.us>
2018-11-21 15:51:31 -0600

    made page-not-found aware of router -- outputs the page tried to request

--- src/app/views/misc/page-not-found/page-not-found.component.ts
+++ src/app/views/misc/page-not-found/page-not-found.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { ScriptStoreService } from '../../../shared/services/script-store/script-store.service';
+import {Component, OnInit} from '@angular/core';
+import {ScriptStoreService} from '../../../shared/services/script-store/script-store.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent implements OnInit {
+export class PageNotFoundComponent implements OnInit
+{
 
-  private scripts = new ScriptStoreService();
+  private _scripts = new ScriptStoreService();
+  protected here: string;
 
-  constructor() {}
+  constructor(route: ActivatedRoute)
+  {
+    this.here = '/' + route.snapshot.url.join('/');
+  }
 
-  ngOnInit() {
-    this.scripts.load('tv-static').then(data => {
+  ngOnInit()
+  {
+    this._scripts.load('tv-static').then(data => {
       console.log('script loaded ', data);
     }).catch(error => console.log(error));
   }
