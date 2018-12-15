34ef99db9230668280ba7ae05bda4a9ee9fd5418
Robert H. Hughes <robert@tenebris.us>
2018-12-15 15:15:19 -0600

    added intentionally-left-blank component and wired into prototype page

---
+++ src/app/shared/components/intentionally-blank/intentionally-blank.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-intentionally-blank',
+  template: `
+    <p>intentionally blank</p>
+  `,
+  styleUrls: ['./intentionally-blank.component.scss']
+})
+export class IntentionallyBlankComponent implements OnInit
+{
+
+// ~~-~~-~~-~~-~~ Constructors ~~-~~-~~-~~-~~
+  constructor() { }
+
+
+  ngOnInit()
+  {
+  }
+
+}
