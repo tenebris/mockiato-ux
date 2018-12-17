8fc405bc5442d5fb7de156fc7e2bc0769581a64b
Robert H. Hughes <robert@tenebris.us>
2018-12-16 21:37:50 -0600

    once again it is saving structure on generation...

--- src/app/views/v1/mock-data-generation/mock-data-generation-v1.component.ts
+++ src/app/views/v1/mock-data-generation/mock-data-generation-v1.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {MockDataGeneratorService} from '../../../shared/services/mock-data-generator/mock-data-generator.service';
 import {MockDataFormGroupService} from '../../../shared/services/mock-data-generator/form/mock-data-form-group.service';
 import {FormControl, FormGroup} from '@angular/forms';
+import {SavedStructure} from '../../../shared/services/mock-data-generator/form/load-saved-structure/load-saved-structure.component';
 
 
 @Component({
@@ -33,6 +34,8 @@ export class MockDataGenerationV1Component implements OnInit
   {
     this.submitted = true;
 
+    SavedStructure.writeLast(this._root.get('structure').value);
+
     const value = this._root.value;
     this.results = this.generator.generate({itemCount: value.itemCount});
   }
@@ -51,7 +54,8 @@ export class MockDataGenerationV1Component implements OnInit
       fileName: new FormControl('mock-data'),
       fileType: new FormControl('json'),
       itemCount: new FormControl(10),
-  })
+      structure: new FormControl(undefined),
+    })
     ;
 
     this._resetForm = new FormGroup({});
