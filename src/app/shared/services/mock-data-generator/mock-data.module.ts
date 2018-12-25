3bea5cf608bb5d506d64abc9e3ba8f725ae81331
Robert H. Hughes <robert@tenebris.us>
2018-12-24 19:42:13 -0600

    basic drag-n-drop reordering of mock-data elements -- todo: emit event to allow debug-display to refresh at root

--- src/app/shared/services/mock-data-generator/mock-data.module.ts
+++ src/app/shared/services/mock-data-generator/mock-data.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import {CoreModule} from '../../core.module';
 
@@ -23,6 +24,7 @@ import {MockDataFormComponent} from './form/mock-data-form.component';
     CommonModule,
     ReactiveFormsModule,
     CoreModule,
+    DragDropModule,
   ],
   exports: [
     LoadSavedStructureComponent,
