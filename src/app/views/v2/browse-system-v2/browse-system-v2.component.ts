65d55fc02a10cd6c2c424136e098e1ed6c372dca
Robert H. Hughes <robert@tenebris.us>
2019-01-19 10:06:46 -0600

    baseline mat-table view for server-list

--- src/app/views/v2/browse-system-v2/browse-system-v2.component.ts
+++ src/app/views/v2/browse-system-v2/browse-system-v2.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ServiceStore} from '../../../shared/model/service/service-store';
+import {ServiceType} from '../../../shared/model/service/service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-browse-system-v2',
   templateUrl: './browse-system-v2.component.html',
   styleUrls: ['./browse-system-v2.component.scss']
 })
-export class BrowseSystemV2Component implements OnInit {
+export class BrowseSystemV2Component implements OnInit
+{
+  displayedColumns: string[] = ['name', 'type', 'group', 'owner', 'when'];
 
-  constructor() { }
 
-  ngOnInit() {
+// ~~-~~-~~-~~-~~ Constructors ~~-~~-~~-~~-~~
+
+  constructor(public serviceStore: ServiceStore) {}
+
+
+  doRefreshServices() { this.serviceStore.publishDummyData(); }
+
+
+  typeName(type: ServiceType): string { return ServiceType[type]; }
+
+
+  formatDate(timestamp: Date): string
+  {
+    return moment(timestamp).format('lll [GMT]Z');
   }
+
+
+  ngOnInit() {}
+
 
 }
