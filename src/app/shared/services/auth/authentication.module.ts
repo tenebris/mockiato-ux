23adb3c0395ac75c7b032c8ef476a3eb6e5599e1
Robert H. Hughes <robert@tenebris.us>
2019-01-19 20:20:58 -0600

    removed unused import

--- src/app/shared/services/auth/authentication.module.ts
+++ src/app/shared/services/auth/authentication.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthenticationService} from './authentication-service';
 import {Auth0AuthenticationService} from './auth0/auth0-authentication.service';
-import {ModalService} from '../modal-service/modal.service';
 
 
 @NgModule({
