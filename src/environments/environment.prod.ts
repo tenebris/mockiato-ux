f3e1225149ac00aa6751d594e82696fdf0bc244d
Robert H. Hughes <robert@tenebris.us>
2019-01-19 19:32:12 -0600

    changed default log-level in dev to debug instead of trace

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import {EnvironmentConfiguration} from '../app/shared/model/common/environment-configuration';
 
 
+// noinspection JSUnusedGlobalSymbols
 export const environment: EnvironmentConfiguration = {
   production: true,
   logLevel: 3,  // see LogLevel in src/app/shared/services/logging/logging.service.ts
