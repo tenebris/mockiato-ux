962f472533469479b061afccf0fe4f75de55007e
Robert H. Hughes <robert@tenebris.us>
2019-01-19 18:31:45 -0600

    refined the service-list sorting abilities

--- src/app/views/v2/browse-system-v2/browse-system-v2.component.ts
+++ src/app/views/v2/browse-system-v2/browse-system-v2.component.ts
@@ -1,12 +1,51 @@
 import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {FormControl} from '@angular/forms';
 import * as moment from 'moment';
 
 import {ServiceStore} from '../../../shared/model/service/service-store';
 import {Service, ServiceType} from '../../../shared/model/service/service';
 import {LastModifiedDetail} from '../../../shared/model/common/last-modified-detail';
 import {appLogger} from '../../../shared/app-logger';
-import {FormControl} from '@angular/forms';
+
+
+function filterPredicate(t: Service, filter: string): boolean
+{
+  return t && t.name.toLowerCase().indexOf(filter) >= 0
+         || t.group.name.toLowerCase().indexOf(filter) >= 0
+         || ServiceType[t.type].toLocaleLowerCase().indexOf(filter) >= 0
+         || (t.owner && t.owner.name.toLowerCase().indexOf(filter) >= 0)
+         || formatDate(t.lastModified).toLowerCase().indexOf(filter) >= 0;
+}
+
+
+function sortingDataAccessor(item: Service, property: string)
+{
+  let value;
+  switch (property)
+  {
+    case 'owner':
+      value = item.owner ? item.owner.name : undefined;
+      break;
+
+    case 'when':
+      value = item.lastModified ? item.lastModified.timestamp : undefined;
+      break;
+
+    case 'group':
+      value = item.group.name;
+      break;
+
+    default:
+      value = item[property];
+  }
+  appLogger().trace('sort accessor', property, value, item);
+  return value;
+}
+
+
+function formatDate(detail: LastModifiedDetail): string
+{ return detail ? moment(detail.timestamp).format('lll [GMT]Z') : ''; }
 
 
 @Component({
@@ -36,29 +75,27 @@ export class BrowseSystemV2Component implements OnInit
   typeName(type: ServiceType): string { return ServiceType[type]; }
 
 
-  formatDate(detail: LastModifiedDetail): string
-  { return detail ? moment(detail.timestamp).format('lll [GMT]Z') : ''; }
-
-
   applyFilter(filterValue: string)
   { this.dataSource.filter = filterValue.trim().toLowerCase(); }
 
 
+  formatDate(lastModified: LastModifiedDetail) { return formatDate(lastModified); }
+
+
   private _configureDataSource()
   {
-    this.dataSource.sort = this.sort;
+    this.dataSource.sortingDataAccessor = sortingDataAccessor;
+    this.dataSource.filterPredicate = filterPredicate;
     this.dataSource.paginator = this.paginator;
-
-    this.dataSource.filterPredicate = (t: Service, filter: string): boolean => {
-      return t && t.name.toLowerCase().indexOf(filter) >= 0
-             || t.group.name.toLowerCase().indexOf(filter) >= 0
-             || ServiceType[t.type].toLocaleLowerCase().indexOf(filter) >= 0;
-    };
+    this.dataSource.sort = this.sort;
   }
 
 
   ngOnInit()
   {
+    this.sort.active = 'when';
+    this.sort.direction = 'desc';
+
     this.serviceStore.services.subscribe((t) => {
       appLogger().debug('detected service list update', t);
       this.dataSource = new MatTableDataSource(t);
