8fc7ce39e4582b08cc39e880b193686249cbc7fd
Robert H. Hughes <robert@tenebris.us>
2019-01-19 19:32:12 -0600

    changed default log-level in dev to debug instead of trace

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 import {EnvironmentConfiguration} from '../app/shared/model/common/environment-configuration';
-
-
-export const environment: EnvironmentConfiguration = {
-  production: false,                              // guess... :)
-  logLevel: 0,                                    // see LogLevel in src/app/shared/services/logging/logging.service.ts
-  coreBaseUrl: 'https://tenebris-mockiato.ngrok.io',           // base-url to use when referencing the core
-  internalAuthModal: false,                       // enable if using internal authentication modal
-};
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -19,4 +10,13 @@ export const environment: EnvironmentConfiguration = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';
+
+
+export const environment: EnvironmentConfiguration = {
+  production: false,                                 // guess... :)
+  logLevel: 1,                                       // see LogLevel in src/app/shared/services/logging/logging.service.ts
+  coreBaseUrl: 'https://tenebris-mockiato.ngrok.io', // base-url to use when referencing the core
+  internalAuthModal: false,                          // enable if using internal authentication modal
+};
+
