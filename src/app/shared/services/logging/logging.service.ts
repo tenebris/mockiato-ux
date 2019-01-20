e1f6b01b685a75117ff644821eacb587a0e6e88c
Robert H. Hughes <robert@tenebris.us>
2019-01-20 13:52:59 -0600

    allow log-level to be overridden for a section of code

--- src/app/shared/services/logging/logging.service.ts
+++ src/app/shared/services/logging/logging.service.ts
@@ -11,12 +11,49 @@ export abstract class Logger
   error: any;
   critical: any;
 
-  protected readonly level: LogLevel = environment.hasOwnProperty('logLevel') ? environment['logLevel'] : LogLevel.TRACE;
+  private readonly _logLevelHistory: LogLevel[] = [];
 
+  protected level: LogLevel = environment.hasOwnProperty('logLevel') ? environment['logLevel'] : LogLevel.WARN;
   protected readonly noop = (): any => undefined;
 
 
   public shouldLogMessage(_input: LogLevel): boolean { return this.level <= _input; }
+
+
+  // noinspection JSUnusedGlobalSymbols
+  /**
+   * Restores the last pushed value for the logging level in effect.
+   */
+  public popLogLevel(): void
+  {
+    if (this._logLevelHistory.length === 0)
+    {
+      this.warn('requested to pop log-level with no history '
+                + '-- resetting to environment default', environment.logLevel);
+
+      this.level = environment.logLevel;
+      return;
+    }
+
+    this.level = this._logLevelHistory.unshift();
+    this.debug('restored log level: ', this.level, this._logLevelHistory);
+  }
+
+
+  // noinspection JSUnusedGlobalSymbols
+  /**
+   * Pushed the current log-level on the stack and sets it to the specified level.
+   *
+   * This is intended to temporally override the log-level from the environment for debugging purposes.
+   *
+   * @param newLevel to use for log statements.
+   */
+  public pushLogLevel(newLevel: LogLevel): void
+  {
+    this.debug(`changing log level to ${newLevel} and pushing old value: ${this.level}`);
+    this._logLevelHistory.push(this.level);
+    this.level = newLevel;
+  }
 
 }
 
